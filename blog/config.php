a5bd1474c474712fc8dc1c509eaf8a2915bb5336
themarioga <marioga2512@gmail.com>
2013-07-04 01:22:49 +0200

    Update config.php

--- blog/config.php
+++ blog/config.php
@@ -1,6 +1,6 @@
 <?php
 	define('DB_HOST', 'localhost');
-    define('DB_USER', 'spickly_spickly');
-    define('DB_PASSWORD', 's.p.i.c.k.l.y');
-    define('DB_DATABASE', 'spickly_blog');
+    define('DB_USER', '');
+    define('DB_PASSWORD', '');
+    define('DB_DATABASE', '');
 ?>
